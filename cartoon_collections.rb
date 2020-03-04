f100548797b6fbc4e1f54d63603e2210e8565fba
Trevor Jones <tjones009@yahoo.com>
2020-03-04 22:09:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
 def roll_call_dwarves(gang)
-  # code an argument here
-  # Your code here
+
   gang.each_with_index do |dwarf, index|
     puts "#{index + 1} #{dwarf}"
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.any? {|call| call.length > 4}
+end
+
+
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
