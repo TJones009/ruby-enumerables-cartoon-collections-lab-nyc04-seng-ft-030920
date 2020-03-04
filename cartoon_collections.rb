4b72096cecf4390d4c01b38e312c7ee7f18b013a
Trevor Jones <tjones009@yahoo.com>
2020-03-04 22:07:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(gang)
   # Your code here
   gang.each_with_index do |dwarf, index|
     puts "#{index + 1} #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
